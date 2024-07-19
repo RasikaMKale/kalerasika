7fc3c39cad538a937011fb5bcc1c21eabcd6bd09
RasikaMKale <rasikamkale@gmail.com>
2024-07-19 10:12:27 +0530

    added files

---
+++ sketch.js
@@ -0,0 +1,20 @@
+function setup() {
+  createCanvas(700, 480);
+  
+  // R, G & B VALUES.
+  background(00, 00, 00);
+  
+  // take a big brush
+  strokeWeight(20);
+  // dip it in the color purple
+  stroke('pink')
+} 
+
+
+function draw() {
+//declare variables
+  let posY = mouseX
+  let posX = mouseY
+  // place a dot on a screen
+point(posY, posX);
+}
